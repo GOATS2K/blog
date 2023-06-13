280ee8699e7ab33188bd669a3525fa4006f69aa0
GOATS2K <jp@glattetre.com>
2023-06-13 14:30:49 +0000

    fix: change giscus theme when main theme changes

---
+++ assets/js/appearance.js
@@ -0,0 +1,86 @@
+const sitePreference = document.documentElement.getAttribute("data-default-appearance");
+const userPreference = localStorage.getItem("appearance");
+
+function getCSSValue(varName) {
+  var cssValue = window.getComputedStyle(document.documentElement).getPropertyValue(varName);
+  return "rgb(" + cssValue.replace(/\s+/g, "") + ")";
+}
+
+function setThemeColor() {
+  var metaThemeColor = document.querySelector("meta[name=theme-color]");
+  document.documentElement.classList.contains("dark")
+    ? metaThemeColor.setAttribute("content", getCSSValue("--color-neutral-800"))
+    : metaThemeColor.setAttribute("content", getCSSValue("--color-neutral"));
+  return true;
+}
+
+if ((sitePreference === "dark" && userPreference === null) || userPreference === "dark") {
+  document.documentElement.classList.add("dark");
+  setThemeColor();
+}
+
+if (document.documentElement.getAttribute("data-auto-appearance") === "true") {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches &&
+    userPreference !== "light"
+  ) {
+    document.documentElement.classList.add("dark");
+    setThemeColor();
+  }
+  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
+    if (event.matches) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    setThemeColor();
+  });
+}
+
+function setGiscusTheme(theme) {
+  console.log("Setting Giscus theme:", theme)
+  function sendMessage(message) {
+    const iframe = document.querySelector('iframe.giscus-frame');
+    if (!iframe) return;
+    iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
+  }
+
+  sendMessage({
+    setConfig: {
+      theme: theme
+    }
+  });
+}
+
+function setGiscusThemeOnLoad() {
+  const theme = document.documentElement.classList.contains("dark") ? "dark" : "light";
+  const interval = setInterval(() => {
+    const iframe = document.querySelector('iframe.giscus-frame');
+    if (iframe) {
+      setGiscusTheme(theme === "dark" ? "dark_dimmed" : "light")
+      clearInterval(interval);
+    }
+  }, 500)
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  setGiscusThemeOnLoad();
+  var switchers = document.querySelectorAll("[id^='appearance-switcher']");
+  switchers.forEach((switcher) => {
+    switcher.addEventListener("click", () => {
+      document.documentElement.classList.toggle("dark");
+      setThemeColor();
+      const theme = document.documentElement.classList.contains("dark") ? "dark" : "light";
+      setGiscusTheme(theme === "dark" ? "dark_dimmed" : "light")
+      localStorage.setItem(
+        "appearance",
+        theme
+      );
+    });
+    switcher.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+      localStorage.removeItem("appearance");
+    });
+  });
+});
